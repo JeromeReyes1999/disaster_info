89c428a9c5490c4eb35fa7be24bedf3ff55cb770
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-19 08:31:44 +0800

    add mount uploader in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
-
+  has_many :comments
   belongs_to :user
+  belongs_to :disaster
+  mount_uploader :image, ImageUploader
 end
