036733aece0db6faa2aa48c82bc3f31b9ea81b89
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-19 11:10:32 +0800

    add new and create for controller

--- app/controllers/admin/disasters_controller.rb
+++ app/controllers/admin/disasters_controller.rb
@@ -6,10 +6,28 @@ class Admin::DisastersController < ApplicationController
     @disasters = Disaster.all
   end
 
+  def new
+    @disaster = Disaster.new
+  end
+
+  def create
+    @disaster = Disaster.new(disaster_params)
+    if @disaster.save
+      redirect_to admin_disasters_path
+    else
+      render :new
+    end
+  end
+
   def check_admin
     unless current_user.admin?
       flash[:notice] = "You do not have permission"
       redirect_to posts_path
     end
   end
+
+  def disaster_params
+    params.require(:disaster).permit(:category)
+  end
+
 end
