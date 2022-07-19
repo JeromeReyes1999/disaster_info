8ece14c60541245c362975a75442ae0306185bd0
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-19 16:02:40 +0800

    add unique number generator for short url

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
   belongs_to :disaster
+
   mount_uploader :image, ImageUploader
+
+
+  before_create :generate_unique_number
+
+  def generate_unique_number
+    loop do
+      @short = rand(0..9999).to_s.rjust(4, '0')
+      break unless Post.exists?(short_url: @short)
+    end
+    self.short_url = @short
+  end
 end
