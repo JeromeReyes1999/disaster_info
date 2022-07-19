3486ea1c325f772ad869132515acd337c1596693
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-19 08:19:16 +0800

    create controller for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,54 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
+  def index
+    @comments = @post.comments.includes(:user)
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_comments_path(@post, @comment)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      redirect_to post_comments_path(@post)
+    end
+  end
+
+  private
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def set_post
+    @post = Post.find params[:post_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+end
